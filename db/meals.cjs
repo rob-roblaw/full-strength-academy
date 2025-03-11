62e1fded2ecc98440129038988eb64199c09fcec
Rob Roblaw <rob.roblaw@wumboinc.com>
2025-03-11 14:02:09 -0700

    initial seeding and error message updates

--- db/meals.cjs
+++ db/meals.cjs
@@ -1,13 +1,13 @@
 const client = require('./client.cjs');
 
-const createMeal = async (mealName, mealFocus, mealCalories, postedByUsername) => {
+const createMeal = async(mealName, mealFocus, mealCalories, postedByUsername) => {
   try {
     await client.query(`
       INSERT INTO meals (name, focus_goal, calories, username)
-      VALUES ('${mealName}', '${mealFocus}', ${mealCalories}, '${postedByUsername});
+      VALUES ('${mealName}', '${mealFocus}', ${mealCalories}, '${postedByUsername}');
     `);
-  } catch (err) {
-    console.log(err);
+  } catch(err) {
+    console.log(`CREATEMEAL ERROR MESSAGE: ${err}`);
   }
 };
 
