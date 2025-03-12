377e485308c25cb6ceff526b2458b873f21d03c8
Rob Roblaw <rob.roblaw@wumboinc.com>
2025-03-12 11:08:27 -0600

    creates routes in app and initial functions in all of the components folders

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+
+  return (
+    <>
+      <h2>Here is our homepage!</h2>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Register.jsx
@@ -0,0 +1,10 @@
+const Register = () => {
+
+  return (
+    <>
+      <h2>Register for an account!</h2>
+    </>
+  )
+}
+
+export default Register
