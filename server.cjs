28cf973b07e9c3be0229e2c15cb144eb92496a23
Rob Roblaw <rob.roblaw@wumboinc.com>
2025-03-12 00:55:32 -0400

    #31 adds get routes by id and focus goal

--- server.cjs
+++ server.cjs
@@ -1,7 +1,7 @@
 const express = require("express");
 const client = require("./db/client.cjs");
 const createExercise = require("./db/exercises.cjs");
-const createMeal = require("./db/meals.cjs");
+const { createMeal , getMealById, getMealByFocusGoal } = require("./db/meals.cjs");
 
 const app = express();
 
@@ -31,6 +31,37 @@ app.post("/api/exercises", async (req, res) => {
   }
 });
 
+// GET /api/meals - MEAL BY iD
+app.get('/api/meals/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const meal = await getMealById(id); 
+    if (meal) {
+      res.status(200).send(meal);
+    } else {
+      res.status(404).send({ message: 'Meal not found' });
+    }
+  } catch (err) {
+    res.status(500).send({ error: `Error retrieving meal: ${err}` });
+  }
+});
+
+app.get('/api/mealsByFocusGoal/:focusGoal', async (req, res) => {
+  const { focusGoal } = req.params;
+
+  try {
+    const meal = await getMealByFocusGoal(focusGoal); 
+    if (meal) {
+      res.status(200).send(meal);
+    } else {
+      res.status(404).send({ message: 'Meal not found' });
+    }
+  } catch (err) {
+    res.status(500).send({ error: `Error retrieving meal: ${err}` });
+  }
+});
+
 // POST /api/meals - TO CREATE MEALS
 app.post('/api/meals', async (req, res) => {
   const { mealName, mealFocus, mealCalories, postedByUsername } = req.body;
@@ -42,6 +73,8 @@ app.post('/api/meals', async (req, res) => {
     res.status(500).send({ error: `Error creating meal: ${err}` });
   }
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 
