67588ddc376a05a3cf3ce57dcfebb48fbb689754
Rob Roblaw <rob.roblaw@wumboinc.com>
2025-03-12 13:46:02 -0400

    for created, bith inputs grab values

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [ newUsername, setNewUsername ] = useState('');
+  const [ newPassword, setNewPassword ] = useState('');
+
+  const addNewUser = (event) => {
+    event.preventDefault(); 
+
+  }
 
   return (
     <>
       <h2>Register for an account!</h2>
+      <form onSubmit={ addNewUser }>
+        <input placeholder="username" onChange={(event) => {setNewUsername(event.target.value)}} />
+        <input placeholder="password" onChange={(event) => {setNewPassword(event.target.value)}} />
+        <button>Create Account</button>
+      </form>
     </>
   )
 }
