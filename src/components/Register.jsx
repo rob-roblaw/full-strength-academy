b808e9225fe0a91fe91f313420f7c7e249437a0c
Rob Roblaw <rob.roblaw@wumboinc.com>
2025-03-13 06:07:45 -0400

    verify passwords match conditional works as expected

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,20 +3,26 @@ import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [ newUsername, setNewUsername ] = useState('');
-  const [ newPassword, setNewPassword ] = useState('');
+  const [ newPassword1, setNewPassword1 ] = useState('');
+  const [ newPassword2, setNewPassword2 ] = useState('');
 
   const navigate = useNavigate();
 
   const addNewUser = async(event) => {
-    event.preventDefault(); 
+    if (newPassword1 === newPassword2) {
+      event.preventDefault(); 
 
-    try {
-      const response = await fetch();
-    } catch(err) {
-      console.log(err);
-    }
+      // try {
+      //   const response = await fetch('https://full-strength-academy.onrender.com/api/auth/register');
+      //   console.log(response);
+      // } catch(err) {
+      //   console.log(err);
+      // }
 
-    navigate('/editprofile');
+      navigate('/editprofile');
+      } else {
+        alert('Passwords must match');
+      }
   }
 
   return (
@@ -24,7 +30,8 @@ const Register = () => {
       <h2>Register for an account!</h2>
       <form onSubmit={ addNewUser }>
         <input placeholder="username" onChange={(event) => {setNewUsername(event.target.value)}} />
-        <input placeholder="password" onChange={(event) => {setNewPassword(event.target.value)}} />
+        <input placeholder="enter password" type="password" onChange={(event) => {setNewPassword1(event.target.value)}} />
+        <input placeholder="verify password" type="password" onChange={(event) => {setNewPassword2(event.target.value)}} />
         <button>Create Account</button>
       </form>
     </>
