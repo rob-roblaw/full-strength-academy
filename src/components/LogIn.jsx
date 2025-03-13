d10942b436b57dcc9f37365c57f8d7264d44dc40
Rob Roblaw <rob.roblaw@wumboinc.com>
2025-03-13 17:56:06 -0400

    login functionality tested with good and bad uns/pws

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [ inputUsername, setInputUsername ] = useState('');
+  const [ inputPassword, setInputPassword ] = useState('');
   const [ token, setToken ] = useState('');
+
+  const navigate = useNavigate();
 
   const loggingIn = async(event) => {
     event.preventDefault();
@@ -15,12 +18,19 @@ const LogIn = () => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          username: username,
-          password: password
+          username: inputUsername,
+          password: inputPassword
         })
       });
-      const userLogin = response.json();
+      const userLogin = await response.json();
       console.log(userLogin);
+      if(userLogin.token === 'undefined') {
+        alert('You did something silly like left your caps lock on or Joe and Brianna reseeded the server')
+      } else {
+        setToken(userLogin.token);
+        localStorage.setItem('token', userLogin.token);
+        navigate('/logs');
+      }
     } catch(err) {
       console.log(err);
     }
@@ -30,8 +40,8 @@ const LogIn = () => {
     <>
       <h2>Log into my account!</h2>
       <form onSubmit={ loggingIn }>
-        <input placeholder='username' onChange={(event) => {setUsername(event.target.value)}} />
-        <input placeholder='password' type='password' onChange={(event) => {setPassword(event.target.value)}} />
+        <input placeholder='username' onChange={(event) => {setInputUsername(event.target.value)}} />
+        <input placeholder='password' type='password' onChange={(event) => {setInputPassword(event.target.value)}} />
         <button>Login</button>
       </form>
     </>
